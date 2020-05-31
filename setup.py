b820c01b2a3b6c784ea1ea1d9b1703db2d38b436
andheroe <freefunning@gmail.com>
2020-05-31 22:18:29 +0300

    basic cli commands

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='seamless',
+    version='0.0.1',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'click~=7.1.2',
+    ],
+    entry_points='''
+        [console_scripts]
+        seamless=seamless:cli
+    ''',
+)
