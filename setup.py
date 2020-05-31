fc3ab896856cb8e8076e8dadcde100a585c1de80
andheroe <freefunning@gmail.com>
2020-06-01 01:12:41 +0300

    more info in setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,38 @@
+import pathlib
+
 from setuptools import setup, find_packages
 
 from core.constants import PACKAGE_NAME, PACKAGE_ENTRY_POINT
 
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
 setup(
     name=PACKAGE_NAME,
     version="0.0.1",
+    description="Command Line Interface to work with the Seamless Cloud product",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/https://github.com/seamless-io/seamless-cli",
+    author="Seamless Cloud",
+    author_email="hello@seamlesscloud.io",
+    license="ISC",
+    classifiers=[
+        "License :: OSI Approved :: ISC License (ISCL)",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+    ],
     packages=find_packages(),
     include_package_data=True,
-    install_requires=["click~=7.1.2",],
+    install_requires=["click~=7.1.2"],
     entry_points="""
         [console_scripts]
         {entry_point}=core.seamless:cli
