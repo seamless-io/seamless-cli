af5bdd1e391a56811c51af0a98582353b199f117
andheroe <freefunning@gmail.com>
2020-06-11 19:09:55 +0300

    Min python 3.6 for f-strings.

--- setup.py
+++ setup.py
@@ -23,7 +23,6 @@ setup(
     classifiers=[
         "License :: OSI Approved :: ISC License (ISCL)",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
