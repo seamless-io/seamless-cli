a9bdc44136d02f87f78f2c47c45ad68dbb143ddd
andheroe <freefunning@gmail.com>
2020-07-20 17:52:44 +0300

    update test project so it can be used as a template for users

--- tests/integration/test_seamless_project/function.py
+++ tests/integration/test_seamless_project/function.py
@@ -1,8 +1,18 @@
-from time import sleep
-import scipy
+import yfinance as yf
 
-if __name__ == '__main__':
-    print(f'SciPy version: {scipy.__version__}')
-    print('Executing...')
-    sleep(1)
-    print('Finished')
+tickers = [
+    yf.Ticker("AAPL"),
+    # ................
+    # ^^ add tickers of companies you want to follow here
+    # you can find a ticker from a company name here https://www.marketwatch.com/tools/quotes/lookup.asp
+]
+
+if __name__ == "__main__":
+    for ticker in tickers:
+        print(f"Company: {ticker.info['longName']}")
+        print(f"Stock Price: {ticker.info['regularMarketPrice']}")
+        if not ticker.recommendations.empty:
+            print("Latest 3 recommendations:")
+            print(ticker.recommendations.iloc[-3:])
+        else:
+            print("There are no recommendations available for this stock")
