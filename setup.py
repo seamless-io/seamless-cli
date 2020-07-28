10a7571a55acb6e89a1cd85f3fc7f83bb9dd0cce
andheroe <freefunning@gmail.com>
2020-07-28 15:36:09 +0300

    fix

--- setup.py
+++ setup.py
@@ -31,6 +31,7 @@ setup(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
     ],
+    packages=find_packages(),
     include_package_data=True,
     install_requires=["click~=7.1.2",
                       "requests~=2.23.0",
